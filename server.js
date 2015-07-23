b4898b13e978a4130dcfc7560933477144a39c7d
John Andrey <andrey_201015@hotmail.com>
2015-07-22 23:35:39 -0500

    Respuesta del servidor con callbacks

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-
+//"use strict";
 
 const http = require('http');
 const port = process.env.PORT || 8080;
 
-const server = http.createServer();
+const server = http.createServer(function(req, res){
+	res.end("Hola io.js");
+});
 
-server.listen(port);
+server.listen(port, function(){
+	console.log("Servidor iniciado en el port " + port);
+});
