596662f8e834f2b74a72e4f1da1179761d63d272
John Andrey <andrey_201015@hotmail.com>
2015-07-22 23:55:17 -0500

    Simplificando el codigo del servidor basico aun mas

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 
-const server = http.createServer(onRequest);
+const server = http.createServer();
 
-server.listen(port, onListening);
+server.listen(port);
+
+server.on('request', onRequest);
+server.on('listening', onListening);
 
 function onRequest(req, res){
 	res.end("Hola io.js");
