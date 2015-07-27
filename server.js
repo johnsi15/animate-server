40adf19f69257edd9c62bf3de96c62e251e570cc
John Andrey <andrey_201015@hotmail.com>
2015-07-27 12:29:10 -0500

    Modificamos el servidor usando un modulo llamado st para comprimir mas el codigo y hacerlo mejor creamos un nuevo archivo llamado index.js

--- server.js
+++ server.js
@@ -3,54 +3,16 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const router = require('./router');
+
 const port = process.env.PORT || 8080;
 
 const server = http.createServer();
 
 server.listen(port);
 
-server.on('request', onRequest);
+server.on('request', router);
 server.on('listening', onListening);
-
-function onRequest(req, res){
-	url = req.url;
-	if(url===('/index') || url === '/'){
-		return serveIndex(res);
-	}
-
-	if(url === '/app.js'){
-		return serveApp(res);
-	}
-
-	res.statusCode = 404;
-	res.end('404 not found '+ url);
-}
-
-function serveIndex(res){
-	index = path.join(__dirname, 'public', 'index.html');
-    res.setHeader('Content-Type', 'text/html');
-
-    rs = fs.createReadStream(index);
-    rs.pipe(res);
-
-    rs.on('error', function(){
-    	res.setHeader('Content-Type', 'text/html');
-    	res.end(err.message);
-    });
-}
-
-function serveApp(res){
-	app = path.join(__dirname, 'public', 'app.js');
-    res.setHeader('Content-Type', 'text/javascript');
-
-    rs = fs.createReadStream(app);
-    rs.pipe(res);
-
-    rs.on('error', function(){
-    	res.setHeader('Content-Type', 'text/javascript');
-    	res.end(err.message);
-    });
-}
 
 function onListening(){
 	console.log("Servidor iniciado en el port " + port);
