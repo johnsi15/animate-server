9c607ed6ac06e1294ae85de2d00860a23cc3ae06
John Andrey <andrey_201015@hotmail.com>
2015-07-31 17:11:57 -0500

    Usamos un nuevo modulo llamado course para manejar las rutas de nuestras entradas

--- router/index.js
+++ router/index.js
@@ -1,17 +1,32 @@
 const path = require('path');
 const st = require('st');
+const course = require('course');
+
+const router = course();
 
 const mount = st({
 	path: path.join(__dirname, '..', 'public'),
-	index: 'index.html'
+	index: 'index.html',
+	passthrough: true //si hay un error de 404 que no se detenga aca sino que siga hasta encontrar el onRequest
 });
 
 function onRequest(req, res){
 	mount(req, res, function(err){
 		if(err) return res.end(err,message);
 
-		res.statusCode = 404;
-		res.end('Not Fount 404'+ url);
+		router(req, res, function(err){
+			if(err) return fail(err,res);
+			
+			res.statusCode = 404;
+			res.end('Not Fount 404'+ req.url);
+		});
 	});
 }
+
+function fail(err, res){
+	res.statusCode = 500;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end(err,message);
+}
+
 module.exports = onRequest;
